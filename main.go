759d0e136ae1ebb90afa6380f7154a9493b2cd5e
KaaB1ade <2507043619@qq.com>
2025-08-06 17:42:21 +0800

    day3 实现前缀树

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
+/*
+(1)
+$ curl -i http://localhost:9999/
+HTTP/1.1 200 OK
+Date: Mon, 12 Aug 2019 16:52:52 GMT
+Content-Length: 18
+Content-Type: text/html; charset=utf-8
+<h1>Hello Gee</h1>
+
+(2)
+$ curl "http://localhost:9999/hello?name=geektutu"
+hello geektutu, you're at /hello
+
+(3)
+$ curl "http://localhost:9999/hello/geektutu"
+hello geektutu, you're at /hello/geektutu
+
+(4)
+$ curl "http://localhost:9999/assets/css/geektutu.css"
+{"filepath":"css/geektutu.css"}
+
+(5)
+$ curl "http://localhost:9999/xxx"
+404 NOT FOUND: /xxx
+*/
+
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/KaaB1ade/go-webserver-frame/gee"
@@ -9,14 +34,22 @@ import (
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
